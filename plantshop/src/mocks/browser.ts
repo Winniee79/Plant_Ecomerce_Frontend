1b76cc9627fa82e37b69ea394643055786db6275
iambngoc <22130187@st.hcmuaf.edu.vn>
2026-01-01 17:39:11 +0700

    Update ui Đăng ký, xử lý lưu thông tin đăng ký vào localStorage

--- plantshop/src/mocks/browser.ts
+++ plantshop/src/mocks/browser.ts
@@ -5,5 +5,6 @@
 // src/mocks/browser.ts
 import { setupWorker } from "msw/browser";
 import { handlers } from "./handlers";
+import { authHandlers } from './handlers/auth.handler';
 
-export const worker = setupWorker(...handlers);
+export const worker = setupWorker(...handlers, ...authHandlers);
